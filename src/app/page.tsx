0ca2b555baffc406627aeb829c1ea587030db7d2
Rutger van Rees <r.vanrees@icloud.com>
2023-05-12 09:40:30 +0200

    Landingspage test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,73 @@
+"use client";
+
 import Image from "next/image";
-import logo from "./assets/logo.svg";
+import logo from "./assets/logo_trans.png";
 
 export default function Home() {
   return (
-    <div className="flex flex-col h-screen w-screen items-center justify-center bg-gradient-to-br from-white to-gray-200">
-      <Image width={450} src={logo} alt="Viszual" className="" />
-      <a
-        href="https://github.com/viszual"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="text-gray-300 hover:text-asphalt transition-all"
-      >
-        <svg
-          width="20"
-          height="20"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
+    <div className="h-screen bg-gradient-to-br from-white to-gray-100">
+      <header className="absolute inset-x-0 top-0 z-50">
+        <nav
+          className="flex items-center justify-between p-6 lg:px-8"
+          aria-label="Global"
         >
-          <g clipPath="url(#clip0_9914_10)">
-            <path
-              d="M12 0C5.374 0 0 5.373 0 12C0 17.302 3.438 21.8 8.207 23.387C8.806 23.498 9 23.126 9 22.81V20.576C5.662 21.302 4.967 19.16 4.967 19.16C4.421 17.773 3.634 17.404 3.634 17.404C2.545 16.659 3.717 16.675 3.717 16.675C4.922 16.759 5.556 17.912 5.556 17.912C6.626 19.746 8.363 19.216 9.048 18.909C9.155 18.134 9.466 17.604 9.81 17.305C7.145 17 4.343 15.971 4.343 11.374C4.343 10.063 4.812 8.993 5.579 8.153C5.455 7.85 5.044 6.629 5.696 4.977C5.696 4.977 6.704 4.655 8.997 6.207C9.954 5.941 10.98 5.808 12 5.803C13.02 5.808 14.047 5.941 15.006 6.207C17.297 4.655 18.303 4.977 18.303 4.977C18.956 6.63 18.545 7.851 18.421 8.153C19.191 8.993 19.656 10.064 19.656 11.374C19.656 15.983 16.849 16.998 14.177 17.295C14.607 17.667 15 18.397 15 19.517V22.81C15 23.129 15.192 23.504 15.801 23.386C20.566 21.797 24 17.3 24 12C24 5.373 18.627 0 12 0Z"
-              fill="currentColor"
+          <div className="flex lg:flex-1">
+            <a href="#" className="-m-1.5 p-1.5">
+              <span className="sr-only">Viszual</span>
+              <Image width={40} height={40} src={logo} alt="Viszual" />
+            </a>
+          </div>
+          <div className="lg:flex lg:flex-1 lg:justify-end">
+            <a
+              href="https://github.com/viszual"
+              target="_blank"
+              className="text-sm font-semibold leading-6 text-gray-800 hover:text-indigo transition"
+            >
+              GitHub <span aria-hidden="true">&rarr;</span>
+            </a>
+          </div>
+        </nav>
+      </header>
+
+      <div className="relative isolate px-6 pt-14 lg:px-8">
+        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56 text-center">
+          <div className="flex items-center justify-center ">
+            <Image
+              width={300}
+              height={300}
+              src={logo}
+              alt="Viszual"
+              className="p-8 mb-8"
             />
-          </g>
-          <defs>
-            <clipPath id="clip0_9914_10">
-              <rect width="24" height="24" fill="white" />
-            </clipPath>
-          </defs>
-        </svg>
-      </a>
+          </div>
+          <h1 className="text-4xl font-bold tracking-tight text-gray-800 sm:text-6xl">
+            Coding the{" "}
+            <span className="rounded-md bg-indigo text-white p-1 px-4 hover:bg-pacific transition">
+              future
+            </span>
+          </h1>
+          <p className="mt-6 text-lg leading-8 text-gray-600">
+            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
+            lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
+            fugiat aliqua.
+          </p>
+          <div className="mt-10 flex items-center justify-center gap-x-6">
+            <a
+              href="mailto:mail@viszual.io"
+              className="rounded-md bg-pacific px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo transition"
+            >
+              Get started
+            </a>
+            <a
+              href="https://twitter.com/viszualio"
+              target="_blank"
+              className="text-sm font-semibold leading-6 text-gray-800 hover:text-indigo transition"
+            >
+              Stay tuned <span aria-hidden="true">→</span>
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
